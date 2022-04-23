05b9c193af65021500821dff2896c11b6a63b2af
Piddos31 <103666254+Piddos31@users.noreply.github.com>
2022-04-23 21:25:53 +0700

    Add files via upload

---
+++ whileLoop.java
@@ -0,0 +1,19 @@
+package com.belajar;
+import java.util.*;
+public class whileLoop {
+    public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+        int a=2;
+        boolean k = true;
+        System.out.println("awal program");
+
+        while (k){
+            System.out.println("aksi ke- " + a);
+            a++;
+            if(a==10){
+                k = false;
+            }
+        }
+        System.out.println("akhir");
+    }
+}
